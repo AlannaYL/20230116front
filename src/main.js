4ef2b6a0d6f540a8407b0e00a7d9619af5ee93e0
Alanna <a04810254@gmail.com>
2023-01-16 15:57:32 +0800

    upload

---
+++ src/main.js
@@ -0,0 +1,25 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+import '@/styles/main.sass'
+
+// 立刻執行function
+(async () => {
+  const app = createApp(App)
+
+  await registerPlugins(app)
+
+  app.mount('#app')
+})()
